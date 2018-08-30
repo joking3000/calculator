7bbc9e8add1a2989d7cd378d52ed357d4ca937d0
Jo King <joozooh@gmail.com>
2018-08-29 19:42:57 -0700

    Add basic operation functions to script

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,18 @@
-let testButton = document.querySelector(".test");
-testButton.addEventListener("click", ()=> console.log("CLICKED THE BUTTON"))
+
+
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
